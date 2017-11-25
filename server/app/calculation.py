f34693397a308b1e57709546a5aba5c970e185f6
Veikko Kovanen <veikko.kovanen@gmail.com>
2017-11-25 15:34:24 +0200

    handle pandas version issues

--- server/app/calculation.py
+++ server/app/calculation.py
@@ -9,5 +9,9 @@ def get_tags(text):
     tf_transformer = TfidfTransformer(use_idf=True, sublinear_tf=True).fit(counts)
     counts_tf = tf_transformer.transform(counts)
     words_df = pd.DataFrame(counts_tf.toarray(), columns=count_vect.get_feature_names())
-    sorted_df = words_df.T.sort(columns=0, ascending=False)
+    
+    if (float(pd.__version__[:4]) < 0.2):
+        sorted_df = words_df.T.sort(columns=0, ascending=False)
+    else:
+        sorted_df = words_df.T.sort_values(by=0, ascending=False)
     return json.loads(sorted_df.head(20).to_json())
